7768b27fcdc9acc07e1796e935fca9ff7946dcb0
Essence Donloe <essenced3@gmail.com>
2024-08-04 14:04:01 -0400

    completed exercise 1

---
+++ Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace PetStore1
+{
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+
+
+    }
+
+}
+
--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput = string.Empty;
+
+            Console.WriteLine("Press 1 to add a product");
+            Console.WriteLine("Type 'exit' to quit");
+            userInput = Console.ReadLine();
+            
+            while (userInput.ToLower() != "exit")
+            {
+                if (userInput == "1")
+                {
+                    DogLeash product = new DogLeash();
+                    {
+                        Console.WriteLine("Enter Product Name");
+                        product.Name = Console.ReadLine();
+                        Console.WriteLine(product.Name);
+                    }
+                }
+
+            }
+
+            
         }
     }
 }
