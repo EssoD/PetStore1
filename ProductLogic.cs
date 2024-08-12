3446e80bc9ab3011d5510f6bdaa317bfccf6cf31
Essence Donloe <essenced3@gmail.com>
2024-08-12 17:15:37 -0400

    completed exercise2

---
+++ ProductLogic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore1
+{
+    public class ProductLogic
+    {
+        private List<Product> _products;
+        private Dictionary<string, DogLeash> _dogleash;
+        private Dictionary<string, CatFood> _catfoods;
+
+        public ProductLogic()
+        {
+            _products = new List<Product>();
+            _dogleash = new Dictionary<string, DogLeash>();
+            _catfoods = new Dictionary<string, CatFood>();
+
+        }
+
+        public void addProduct(Product product)
+        {
+            _products.Add(product);
+
+            if (product is DogLeash)
+            {
+                _dogleash.Add(product.Name, product as DogLeash);
+
+            }
+            else if (product is CatFood)
+            {
+                _catfoods.Add(product.Name, product as CatFood);
+
+            }
+
+        }
+
+        public List<Product> GetAllProducts()
+        {
+         
+               return _products;
+        }
+
+        public DogLeash GetDogLeashByName(string name)
+        {
+
+            return _dogleash[name];
+        }
+
+    }
+}
