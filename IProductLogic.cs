a44eadbc77ca64ddf2a84c7055c221fd66b7e7b9
Essence Donloe <essenced3@gmail.com>
2024-08-30 13:02:53 -0400

    created interface and LinQ

---
+++ IProductLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore1
+{
+    public interface IProductLogic
+    {
+        void addProduct(Product product);
+        List<Product> GetAllProducts();
+        DogLeash GetDogLeashByName(string name);
+        List<Product> GetOnlyInStockProducts();
+
+        List<string> GetOutOfStockProducts();
+        
+
+    }
+}
